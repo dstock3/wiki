8c40df89eeb443b713b4659d00eaca1408e175f2
David Stockdale <dstock3@gmail.com>
2023-08-30 18:06:36 -0500

    Created ArticlePage component and corresponding css.

---
+++ src/pages/ArticlePage.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import Article from '../components/Article';
+import '../styles/ArticlePage.css'; 
+
+const ArticlePage = ({ match }) => {
+  const [articleData, setArticleData] = useState(null);
+
+  useEffect(() => {
+    // Fetch data here based on article ID (match.params.id)
+    // currently using hardcoded data
+    const fetchedData = {
+      title: "Sample Article",
+      content: [
+        { title: "Introduction", text: "This is an introduction." },
+        { title: "Content", text: "This is the main content." }
+      ],
+      references: [
+        { name: "Reference 1", link: "#" },
+        { name: "Reference 2", link: "#" }
+      ]
+    };
+    
+    setArticleData(fetchedData);
+  }, [match.params.id]);
+
+  return (
+    <div className="article-page">
+    {articleData && (
+        <Article
+            title={articleData.title}
+            content={articleData.content}
+            references={articleData.references}
+        />
+    )}
+    </div>
+  );
+};
+
+export default ArticlePage;
