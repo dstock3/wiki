787b8e977b9e2f223365884e7563037813e95538
dstock3 <dstock3@gmail.com>
2023-08-31 14:12:51 -0500

    Added copyright statement and boilerplate for ContactPage component.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,27 +5,33 @@ import '../styles/Footer.css';
 const Footer = () => {
   return (
     <div className="footer-container">
-      <div className="footer-section">
-        <h4>About</h4>
-        <ul>
-          <li><Link to="/about">About [Your Site]</Link></li>
-          <li><Link to="/contact">Contact Us</Link></li>
-          <li><Link to="/disclaimer">Disclaimer</Link></li>
-        </ul>
+      <div className="footer-subcontainer">
+        <div className="footer-section">
+          <h4>About</h4>
+          <ul>
+            <li><Link to="/about">About WikiWise</Link></li>
+            <li><Link to="/contact">Contact Us</Link></li>
+            <li><Link to="/disclaimer">Disclaimer</Link></li>
+          </ul>
+        </div>
+        <div className="footer-section">
+          <h4>Contribute</h4>
+          <ul>
+            <li><Link to="/contribute">How to Contribute</Link></li>
+            <li><Link to="/donate">Donate</Link></li>
+          </ul>
+        </div>
+        <div className="footer-section">
+          <h4>Legal</h4>
+          <ul>
+            <li><Link to="/terms">Terms of Use</Link></li>
+            <li><Link to="/privacy">Privacy Policy</Link></li>
+          </ul>
+        </div>
       </div>
-      <div className="footer-section">
-        <h4>Contribute</h4>
-        <ul>
-          <li><Link to="/contribute">How to Contribute</Link></li>
-          <li><Link to="/donate">Donate</Link></li>
-        </ul>
-      </div>
-      <div className="footer-section">
-        <h4>Legal</h4>
-        <ul>
-          <li><Link to="/terms">Terms of Use</Link></li>
-          <li><Link to="/privacy">Privacy Policy</Link></li>
-        </ul>
+
+      <div className="footer-copyright">
+        © {new Date().getFullYear()} WikiWise, All Rights Reserved.
       </div>
     </div>
   );
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,9 +1,39 @@
+import SideMenu from "../components/SideMenu";
+import "../styles/ContactPage.css"
+
 const ContactPage = () => {
-    return(
-        <div className="contact-page">
-
+    return (
+      <div className="contact-page">
+        <SideMenu />
+        <div className="contact-container">
+            <h1>Contact Us</h1>
+            <p>We'd love to hear from you! Whether you have a question, suggestion, or simply want to give us some feedback, feel free to reach out.</p>
+    
+            <h2>Email</h2>
+            <p>You can contact us directly at: <a href="mailto:support@wikiwise.com">support@wikiwise.com</a></p>
+    
+            <h2>Contact Form</h2>
+            <form>
+            <div className="form-group">
+                <label htmlFor="name">Name</label>
+                <input type="text" id="name" name="name" />
+            </div>
+            
+            <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" />
+            </div>
+            
+            <div className="form-group">
+                <label htmlFor="message">Message</label>
+                <textarea id="message" name="message" rows="5"></textarea>
+            </div>
+            
+            <button type="submit">Submit</button>
+            </form>
         </div>
-    )
-}
-
-export default ContactPage
+      </div>
+    );
+  };
+  
+  export default ContactPage;
