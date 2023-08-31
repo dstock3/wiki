3c079cf1ef3eafcd96dd9d239f30dc84008d3968
dstock3 <dstock3@gmail.com>
2023-08-30 22:15:53 -0500

    made stylistic adjustments to header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,15 +6,22 @@ import SearchBar from './SearchBar';
 const Header = () => {
   return (
     <div className="header-container">
-      <div className="header-logo">
-        <Link to="/">
-          {/* logo here */}
-          <img src="your-logo-here.png" alt="Site Logo" />
-        </Link>
+      <div className="header-subcontainer">
+        <div className="header-menu">
+          {/* menu icon here */}
+          Menu
+        </div>
+        <div className="header-logo">
+          <Link to="/">
+            {/* logo here */}
+            <img src="your-logo-here.png" alt="Site Logo" />
+          </Link>
+        </div>
+        <div className="header-search">
+          <SearchBar />
+        </div>
       </div>
-      <div className="header-search">
-        <SearchBar />
-      </div>
+
       <div className="header-auth">
         <Link to="/create-account">
           <div className="create-account-button">Create Account</div>
