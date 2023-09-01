002161a8de2ec3fec5900fca65a91c2ae71c4ad1
David Stockdale <dstock3@gmail.com>
2023-09-01 17:33:17 -0500

    Added disclaimer boilerplate.

--- src/pages/DisclaimerPage.js
+++ src/pages/DisclaimerPage.js
@@ -1,9 +1,34 @@
+import React from 'react';
+import SideMenu from '../components/SideMenu';
+import '../styles/DisclaimerPage.css';
+
 const DisclaimerPage = () => {
-    return(
-        <div className="disclaimer-page">
+  return (
+    <div className="disclaimer-page">
+      <SideMenu />
+      <div className="disclaimer-container">
+        <h1>Disclaimer</h1>
+        
+        <h2>General Information</h2>
+        <p>The content provided on WikiWise is for informational purposes only and should not be construed as professional advice.</p>
 
-        </div>
-    )
-}
+        <h2>User-Generated Content</h2>
+        <p>WikiWise allows users to contribute content. The service is not responsible for the accuracy, completeness, or reliability of any information provided by users. User-contributed content does not necessarily represent the views or policies of WikiWise.</p>
+        
+        <h2>Accuracy of Information</h2>
+        <p>While we strive for accuracy, we cannot guarantee that all information presented is up to date or free of errors.</p>
 
-export default DisclaimerPage
+        <h2>External Links</h2>
+        <p>Our site may contain links to external websites. We are not responsible for the content or privacy practices of these external sites.</p>
+
+        <h2>Limitation of Liability</h2>
+        <p>We are not liable for any damages arising from the use or inability to use our site.</p>
+
+        <h2>Contact</h2>
+        <p>If you have any questions or concerns about this disclaimer, you can contact us at <a href="mailto:support@wikiwise.com">support@wikiwise.com</a>.</p>
+      </div>
+    </div>
+  );
+};
+
+export default DisclaimerPage;
--- src/pages/PrivacyPage.js
+++ src/pages/PrivacyPage.js
@@ -1,9 +1,37 @@
+import React from 'react';
+import SideMenu from '../components/SideMenu';
+import '../styles/PrivacyPage.css';
+
 const PrivacyPage = () => {
-    return(
-        <div className="privacy-page">
+  return (
+    <div className="privacy-page">
+      <SideMenu />
+      <div className="privacy-container">
+        <h1>Privacy Policy</h1>
+        
+        <h2>Information We Collect</h2>
+        <p>We collect personal information that you voluntarily provide when you use WikiWise, such as your name and email address.</p>
+        
+        <h2>How We Use Your Information</h2>
+        <p>Your information is used to manage your account, provide customer support, and for other purposes described in this policy.</p>
 
-        </div>
-    )
-}
+        <h2>Cookies</h2>
+        <p>We use cookies to enhance your experience on our site. You can disable cookies in your browser, but some features of the site may not function properly as a result.</p>
 
-export default PrivacyPage
+        <h2>Data Security</h2>
+        <p>We take reasonable measures to protect your personal information from unauthorized access.</p>
+
+        <h2>Third-Party Services</h2>
+        <p>We may share your information with third-party services that perform services on our behalf.</p>
+
+        <h2>Changes to This Policy</h2>
+        <p>We may update this Privacy Policy from time to time. We will notify you of any changes by posting the new policy on this page.</p>
+
+        <h2>Contact</h2>
+        <p>If you have any questions or concerns about this Privacy Policy, please contact us at <a href="mailto:support@wikiwise.com">support@wikiwise.com</a>.</p>
+      </div>
+    </div>
+  );
+};
+
+export default PrivacyPage;
