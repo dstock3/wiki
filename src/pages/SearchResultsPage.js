7d6521d71b2fcf400c647756b640dc2be2bd414e
dstock3 <dstock3@gmail.com>
2023-09-08 18:32:44 -0500

    added api call to search result component. added sample data for the time being to test display.

--- src/pages/SearchResultsPage.js
+++ src/pages/SearchResultsPage.js
@@ -1,41 +1,91 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import '../styles/SearchResultsPage.css';
 
 const SearchResultsPage = ({ match }) => {
   const [searchResults, setSearchResults] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  
+  const sampleData = [
+    {
+      title: "The Wonders of Space",
+      excerpt: "Space, the final frontier. Discover the mysteries of the universe, from galaxies and stars to planets and their moons.",
+      image: {
+        source: "https://via.placeholder.com/150x150?text=Space",
+        alt: "Image representing space with stars and galaxies"
+      }
+    },
+    {
+      title: "Deep Ocean Exploration",
+      excerpt: "Venture into the depths of the Earth's oceans. Learn about mysterious creatures that reside in the darkest corners.",
+      image: {
+        source: "https://via.placeholder.com/150x150?text=Ocean",
+        alt: "Image representing the ocean with shades of blue"
+      }
+    },
+    {
+      title: "The History of Computers",
+      excerpt: "From giant mainframes to the smartphone in your pocket, explore the evolution of computers through time.",
+      image: {
+        source: "https://via.placeholder.com/150x150?text=Computer",
+        alt: "Image representing a computer"
+      }
+    },
+    {
+      title: "Mysteries of the Human Mind",
+      excerpt: "Dive deep into the complexities of the human brain and explore consciousness, emotions, and thought processes.",
+      image: {
+        source: "https://via.placeholder.com/150x150?text=Brain",
+        alt: "Image representing a stylized human brain"
+      }
+    }
+  ];
 
   useEffect(() => {
-    // Fetch search results based on query (match.params.query)
-    // currently using hardcoded data
-    const fetchedData = [
-      {
-        title: "Sample Article 1",
-        excerpt: "This is a brief snippet from Sample Article 1...",
-      },
-      {
-        title: "Sample Article 2",
-        excerpt: "Here's another snippet from Sample Article 2...",
-      }
-    ];
-    
-    setSearchResults(fetchedData);
+    // assuming the api endpoint is something like this:
+    /*
+    const apiUrl = `https://your-api-url.com/search?query=${match.params.query}`;
+
+    axios.get(apiUrl)
+      .then(response => {
+        setSearchResults(response.data); // need to update based on API response structure
+        setLoading(false);
+      })
+      .catch(err => {
+        console.error("Error fetching search results:", err);
+        setError(err);
+        setLoading(false);
+      });
+      */
+      setSearchResults(sampleData);
+      setLoading(false);
   }, [match.params.query]);
 
   return (
     <div className="search-results-page">
-      {searchResults.length > 0 ? (
-        searchResults.map((result, index) => (
-          <div key={index} className="search-result">
-            <div className="search-result-image-container">
-              <img src="https://via.placeholder.com/150" alt="Article" />
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Error loading search results. Please try again.</p>
+      ) : searchResults.length > 0 ? (
+        searchResults.map((result, index) => {
+          const { title, excerpt, image = {} } = result;
+          const { source = "https://via.placeholder.com/150", alt = "Article Image" } = image;
+
+          return (
+            <div key={index} className="search-result">
+              <div className="search-result-image-container">
+                <img src={source} alt={alt} />
+              </div>
+              <div className="search-result-container">
+                <h2><Link to={`/${match.params.portalid}/article/${title}`}>{title}</Link></h2>
+                <p>{excerpt}</p>
+              </div>
             </div>
-            <div className="search-result-container">
-              <h2><Link to={`/${match.params.portalid}/article/${result.title}`}>{result.title}</Link></h2>
-              <p>{result.excerpt}</p>
-            </div>
-          </div>
-        ))
+          );
+        })
       ) : (
         <p className="no-results">
           No results found for "{match.params.query}".
@@ -43,6 +93,8 @@ const SearchResultsPage = ({ match }) => {
       )}
     </div>
   );
+
+
 };
 
 export default SearchResultsPage;
