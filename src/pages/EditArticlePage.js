8d5f6baf2affd593184359d051e00f05480cb64f
David Stockdale <dstock3@gmail.com>
2023-09-10 11:21:49 -0500

    made stylistic adjustments to infobox editor.

--- src/pages/EditArticlePage.js
+++ src/pages/EditArticlePage.js
@@ -248,54 +248,72 @@ const EditArticlePage = ({ match }) => {
                 <div className="info-group">
                     <div className="infobox">
                         <h3>Infobox</h3>
-                        <input 
-                            type="text" 
-                            placeholder="Infobox Title" 
-                            value={infobox.title}
-                            onChange={(e) => handleInfoboxChange('title', e.target.value)} 
-                        />
-                        <label>Upload Image:
+                        <div className="info-field-container">
+                            <label>Infobox Title:</label>
                             <input 
-                                type="file" 
-                                accept="image/*"
-                                onChange={handleInfoboxImageUpload} 
+                                type="text" 
+                                placeholder="Infobox Title" 
+                                value={infobox.title}
+                                onChange={(e) => handleInfoboxChange('title', e.target.value)} 
                             />
-                        </label>
-                        <input 
-                            type="text" 
-                            placeholder="Image Alt Text" 
-                            value={infobox.image.alt}
-                            onChange={(e) => handleInfoboxImageChange('alt', e.target.value)} 
-                        />
-                    
-                        {infobox.info.map((infoField, index) => (
-                            <div key={index} className="infoField">
+                        </div>
+                        <div className="info-field-container">
+                            <label>Upload Image:
                                 <input 
-                                    type="text" 
-                                    placeholder="Label" 
-                                    value={infoField.label}
-                                    onChange={(e) => handleInfoboxInfoChange(index, 'label', e.target.value)} 
+                                    type="file" 
+                                    accept="image/*"
+                                    onChange={handleInfoboxImageUpload} 
                                 />
-                                {
-                                    infoField.header ? (
-                                        <span>Header</span>
-                                    ) : (
+                            </label>
+                        </div>
+                        <div className="info-field-container">
+                            <label>Image Description: </label>
+                            <input 
+                                type="text" 
+                                placeholder="Image Alt Text" 
+                                value={infobox.image.alt}
+                                onChange={(e) => handleInfoboxImageChange('alt', e.target.value)} 
+                            />
+                        </div>
+                        <div className="label-value-pairs">
+                            {infobox.info.map((infoField, index) => (
+                                <div key={index} className="label-value-pair">
+                                    <div className="info-field-container">
+                                        <label>Info Field {index + 1}:</label>
                                         <input 
                                             type="text" 
-                                            placeholder="Value" 
-                                            value={infoField.value}
-                                            onChange={(e) => handleInfoboxInfoChange(index, 'value', e.target.value)} 
+                                            placeholder="Label" 
+                                            value={infoField.label}
+                                            onChange={(e) => handleInfoboxInfoChange(index, 'label', e.target.value)} 
                                         />
-                                    )
-                                }
-                                <input 
-                                    type="checkbox" 
-                                    checked={infoField.header || false}
-                                    onChange={(e) => handleInfoboxInfoChange(index, 'header', e.target.checked)}
-                                /> Mark as Header
-                            </div>
-                        ))}
-                        <button className="add-infofield-button" onClick={addInfoField}>+</button>
+                                    </div>
+                                    {
+                                        infoField.header ? (
+                                            <span>Header</span>
+                                        ) : (
+                                            <div className="info-field-container">
+                                                <label>Value:</label>
+                                                <input 
+                                                    type="text" 
+                                                    placeholder="Value" 
+                                                    value={infoField.value}
+                                                    onChange={(e) => handleInfoboxInfoChange(index, 'value', e.target.value)} 
+                                                />
+                                            </div>
+                                        )
+                                    }
+                                    <div className="info-checkbox-container">
+                                        <input 
+                                            type="checkbox" 
+                                            checked={infoField.header || false}
+                                            onChange={(e) => handleInfoboxInfoChange(index, 'header', e.target.checked)}
+                                        /> <span>Mark as Header</span>
+                                    </div>
+                                </div>
+                            ))}
+                            <button className="add-infofield-button" onClick={addInfoField}>+</button>
+                        </div>
+                        
                     </div>
                     <div className="ref-group">
                         <label className="main-label">Article References:</label>
