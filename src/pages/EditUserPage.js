dadb1b6bfd320e067b8d936b7f3fc3ce4255944a
David Stockdale <dstock3@gmail.com>
2023-09-11 17:54:52 -0500

    Fleshed out EditUserPage and corresponding css.

--- src/pages/EditUserPage.js
+++ src/pages/EditUserPage.js
@@ -1,9 +1,80 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import '../styles/EditUserPage.css';
 
-const EditUserPage = () => {
+const EditUserPage = ({ match }) => {
+  const [userData, setUserData] = useState({
+    email: '',
+    bio: ''
+  });
+
+  useEffect(() => {
+    document.title = `WikiWise | Edit ${match.params.username}`;
+    
+    const fetchedUserData = {
+      email: 'johndoe@example.com',
+      bio: 'A passionate writer and nature enthusiast.'
+    };
+
+    setUserData(fetchedUserData);
+  }, [match.params.username]);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setUserData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // send the modified userData to backend
+    console.log("Updated user data:", userData);
+  };
+
   return (
-    <div>EditUserPage</div>
-  )
-}
+    <div className="edit-user-page">
+      <h2>Edit Profile</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="input-group">
+          <label className="user-label" htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={userData.email}
+            className="user-input"
+            onChange={handleChange}
+          />
+        </div>
 
-export default EditUserPage
+        <div className="input-group bio-group">
+          <label className="user-label" htmlFor="bio">Bio:</label>
+          <textarea
+            id="bio"
+            name="bio"
+            value={userData.bio}
+            className="user-input"
+            onChange={handleChange}
+            rows="4"
+          />
+        </div>
+
+        <div className="input-group">
+          <label className="user-label" htmlFor="password">New Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className="user-input"
+            onChange={handleChange}
+          />
+        </div>
+        
+        <button className="user-submit-button" type="submit">Update Profile</button>
+      </form>
+    </div>
+  );
+};
+
+export default EditUserPage;
