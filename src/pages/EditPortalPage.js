97305e9d42bbf83045e5555be5c619f267572634
David Stockdale <dstock3@gmail.com>
2023-09-16 15:46:34 -0500

    made stylistic adjustments to Edit Portal Page.

--- src/pages/EditPortalPage.js
+++ src/pages/EditPortalPage.js
@@ -5,8 +5,10 @@ const EditPortalPage = ({ match, history }) => {
   const [portalData, setPortalData] = useState({
     portalTitle: '',
     portalDescription: '',
-    portalImage: ''
+    portalImage: '',
+    portalImageFile: null
   });
+  
   const [loading, setLoading] = useState(true);
   const isEditMode = !!match.params.portalid;
 
@@ -23,19 +25,53 @@ const EditPortalPage = ({ match, history }) => {
     setLoading(false);
   }, [match.params.portalid]);
 
+  const handleImageUpload = e => {
+    const file = e.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            setPortalData(prevData => ({
+                ...prevData,
+                portalImage: reader.result,
+                portalImageFile: file
+            }));
+        }
+        reader.readAsDataURL(file);
+    }
+  };
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setPortalData(prevState => ({ ...prevState, [name]: value }));
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if (isEditMode) {
-      // update portal in backend
-    } else {
-      // create new portal in backend
+
+    let formData = new FormData();
+    formData.append('portalTitle', portalData.portalTitle);
+    formData.append('portalDescription', portalData.portalDescription);
+    formData.append('image', portalData.portalImageFile);
+
+    try {
+        if (isEditMode) {
+            // update portal in backend
+            // Depending on your API, you might use a PUT or PATCH request here
+            await fetch(`/api/portals/${match.params.portalid}`, {
+                method: 'PUT',
+                body: formData
+            });
+        } else {
+            // create new portal in backend
+            await fetch('/api/portals', {
+                method: 'POST',
+                body: formData
+            });
+        }
+        history.push(isEditMode ? `/${match.params.portalid}` : "/");
+    } catch (error) {
+        console.error("Error uploading image:", error);
     }
-    history.push(isEditMode ? `/${match.params.portalid}` : "/");
   };
 
   const handleDelete = () => {
@@ -51,47 +87,61 @@ const EditPortalPage = ({ match, history }) => {
 
   return (
     <div className="edit-portal-page">
-      <h1>{isEditMode ? "Edit Portal" : "Create Portal"}</h1>
-      <form onSubmit={handleSubmit}>
-        {isEditMode && (
-          <button type="button" onClick={handleDelete} className="delete-button">
-            Delete Portal
-          </button>
-        )}
-        <div className="form-group">
-          <label>Title:</label>
-          <input 
-            type="text" 
-            name="portalTitle" 
-            value={portalData.portalTitle} 
-            onChange={handleChange}
-            required
-          />
-        </div>
+      <div className="edit-portal-container">
+        <h1 className="edit-portal-header">
+          {isEditMode ? "Edit Portal" : "Create Portal"}
+        </h1>
+        <form className="portal-form" onSubmit={handleSubmit}>
+          <div className="portal-form-group-container">
+            <div className="portal-form-group title-group">
+              <label className="portal-main-label">Title:</label>
+              <input 
+                type="text" 
+                name="portalTitle" 
+                value={portalData.portalTitle} 
+                onChange={handleChange}
+                required
+              />
+            </div>
 
-        <div className="form-group">
-          <label>Description:</label>
-          <textarea 
-            name="portalDescription" 
-            value={portalData.portalDescription} 
-            onChange={handleChange}
-            required
-          />
-        </div>
+            <div className="portal-form-group img-form-group">
+              <div className="img-upload-container">
+                <label className="portal-main-label">Upload Image:</label>
+                <input 
+                    type="file" 
+                    name="portalImageFile" 
+                    onChange={handleImageUpload}
+                />
+              </div>
+              {portalData.portalImage && (
+                  <img src={portalData.portalImage} className="img-preview" alt="Uploaded Preview" width="100" />
+              )}
+            </div>
+          </div>
 
-        <div className="form-group">
-          <label>Image URL:</label>
-          <input 
-            type="url" 
-            name="portalImage" 
-            value={portalData.portalImage} 
-            onChange={handleChange}
-          />
-        </div>
+          <div className="portal-form-group">
+            <label className="portal-main-label">Description:</label>
+            <textarea 
+              name="portalDescription" 
+              value={portalData.portalDescription} 
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-        <button type="submit">Save</button>
-        <button type="button" onClick={() => history.goBack()}>Cancel</button>
-      </form>
+          <div className="button-container">
+            <div className="submit-container">
+              <button type="submit" className="edit-portal-button">Save</button>
+              <button type="button" onClick={() => history.goBack()} className="edit-portal-button">Cancel</button>
+            </div>
+            {isEditMode && (
+              <button type="button" onClick={handleDelete} className="delete-button">
+                Delete Portal
+              </button>
+            )}
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
