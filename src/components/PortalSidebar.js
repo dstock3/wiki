71a9d4524fb984c1e721929a40cbc7570567d77b
David Stockdale <dstock3@gmail.com>
2023-09-16 14:32:51 -0500

    added PortalSidebar component which should be populated with articles from that particular portal. Made stylistic adjustments.

---
+++ src/components/PortalSidebar.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+
+const PortalSidebar = ({articles}) => {
+  return (
+    <div className="side-menu-container">
+      <h3>Articles</h3>
+      <ul>
+        {articles.slice(0, 10).map((article, index) => (
+          <li key={index}>
+            <a href={article.link}>{article.title}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default PortalSidebar;
--- src/pages/PortalHomePage.js
+++ src/pages/PortalHomePage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import '../styles/PortalHomePage.css';
+import PortalSidebar from '../components/PortalSidebar';
 
 const PortalHomePage = ({ match }) => {
   const [portalData, setPortalData] = useState(null);
@@ -13,7 +14,12 @@ const PortalHomePage = ({ match }) => {
     
     const fetchedPortalData = {
       portalTitle: "Nature & Wildlife",
-      portalDescription: "This portal is dedicated to all things nature and wildlife.",
+      portalDescription: "This portal is dedicated to all things nature and wildlife. From the smallest insect to the largest mammal, we have it all! We also have a special section for plants and fungi. If you're interested in learning more about the natural world, you've come to the right place! We have articles on everything from the common house cat to the rarest of birds. We also have articles on plants and fungi, so if you're interested in learning more about the natural world, you've come to the right place!",
+      articles: [
+        { title: "Peregrine Falcon", link: "/article/peregrine-falcon" },
+        { title: "Mountain Lion", link: "/article/mountain-lion" },
+        // ... more articles
+      ],
       featuredArticle: {
         title: "Peregrine Falcon",
         summary: "A bird of prey renowned for its speed...",
@@ -31,31 +37,33 @@ const PortalHomePage = ({ match }) => {
     <div className="portal-home-page">
       {portalData && (
         <>
-          <h1>{portalData.portalTitle}</h1>
-          <hr />
-          <div className="portal-description">
-            <p>{portalData.portalDescription}</p>
-            <div className="portal-image">
-              <img src="https://via.placeholder.com/250" alt="placeholder" />
+          <PortalSidebar articles={portalData.articles} />
+          <div className="portal-home-container">
+            <h1>{portalData.portalTitle}</h1>
+            <hr />
+            <div className="portal-description">
+              <p>{portalData.portalDescription}</p>
+              <div className="portal-image">
+                <img src="https://via.placeholder.com/250" alt="placeholder" />
+              </div>
+            </div>
+            <div className="featured-article">
+              <h2>Featured Article: {portalData.featuredArticle.title}</h2>
+              <p>{portalData.featuredArticle.summary}</p>
+              <a href={portalData.featuredArticle.link}>Read More</a>
+            </div>
+            
+            <div className="recent-updates">
+              <h3>Recent Updates</h3>
+              <ul>
+                {portalData.recentUpdates.map((article, index) => (
+                  <li key={index}>
+                    <a href={article.link}>{article.title}</a>
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
-          <div className="featured-article">
-            <h2>Featured Article: {portalData.featuredArticle.title}</h2>
-            <p>{portalData.featuredArticle.summary}</p>
-            <a href={portalData.featuredArticle.link}>Read More</a>
-          </div>
-          
-          <div className="recent-updates">
-            <h3>Recent Updates</h3>
-            <ul>
-              {portalData.recentUpdates.map((article, index) => (
-                <li key={index}>
-                  <a href={article.link}>{article.title}</a>
-                </li>
-              ))}
-            </ul>
-          </div>
-          
         </>
       )}
     </div>
