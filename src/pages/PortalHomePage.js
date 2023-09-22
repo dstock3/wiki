a5c64d9dc6ddcd686c0353374fbc84b7b93441e8
David Stockdale <dstock3@gmail.com>
2023-09-22 10:04:31 -0500

    made stylistic adjustments.

--- src/pages/PortalHomePage.js
+++ src/pages/PortalHomePage.js
@@ -14,38 +14,6 @@ const PortalHomePage = ({ match, endpoint }) => {
     document.title = `WikiWise | Portal Home`;
   }, []);
 
-  /*
-
-  useEffect(() => {
-    // fetch portal data from API based on portal ID
-
-    const fetchedPortalData = {
-      portalTitle: "Nature & Wildlife",
-      portalDescription: "This portal is dedicated to all things nature and wildlife. From the smallest insect to the largest mammal, we have it all! We also have a special section for plants and fungi. If you're interested in learning more about the natural world, you've come to the right place! We have articles on everything from the common house cat to the rarest of birds. We also have articles on plants and fungi, so if you're interested in learning more about the natural world, you've come to the right place!",
-      portalImage: {
-        src: "https://via.placeholder.com/250",
-        alt: "placeholder image"
-      },
-      articles: [
-        { title: "Peregrine Falcon", link: "/article/peregrine-falcon" },
-        { title: "Mountain Lion", link: "/article/mountain-lion" },
-        // ... more articles
-      ],
-      featuredArticle: {
-        title: "Peregrine Falcon",
-        summary: "A bird of prey renowned for its speed and agility, the peregrine falcon is one of the most widespread birds in the world. It is found on every continent except Antarctica, and it is the fastest animal on Earth. It can reach speeds of up to 200 miles per hour when diving for prey.",
-        link: "/nature-wildlife/article/peregrine-falcon"
-      },
-      recentUpdates: [
-        { title: "Mountain Lion", link: "/nature-wildlife/article/mountain-lion" },
-      ]
-    };
-
-    setPortalData(fetchedPortalData);
-
-  }, [match.params.portalid]);
-  */
-
   useEffect(() => {
     axios.get(`${endpoint}/portals/${match.params.portalid}`)
       .then(response => {
