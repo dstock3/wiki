7d5d25ad960fb2ad8a1ccb05897c0c72baffae9b
David Stockdale <dstock3@gmail.com>
2023-09-22 09:53:53 -0500

    refactored PortalsPage component to account for data structure of portals schema.

--- src/pages/PortalsPage.js
+++ src/pages/PortalsPage.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const PortalsPage = ({endpoint}) => {
+const PortalsPage = ({ endpoint }) => {
     const [portals, setPortals] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
@@ -9,30 +9,33 @@ const PortalsPage = ({endpoint}) => {
     useEffect(() => {
         document.title = `WikiWise | Portals`;
         axios.get(`${endpoint}/portals/`)
-        .then(response => {
-            setPortals(response.data);
-            setLoading(false);
-        })
-        .catch(err => {
-            setError(err.message);
-            setLoading(false);
-        });
-        console.log(portals)
+            .then(response => {
+                setPortals(response.data);
+                setLoading(false);
+            })
+            .catch(err => {
+                setError(err.message);
+                setLoading(false);
+            });
     }, []);
 
-    return(
+    if (loading) return <div className="portals-page">Loading...</div>;
+    if (error) return <div className="portals-page">Error: {error}</div>;
+
+    return (
         <div className="portals-page">
             <h1>Portals</h1>
             <div className="portals-container">
                 {portals && portals.map((portal) => (
-                    <div className="portal-card" key={portal.id}>
-                        <h2>{portal.title}</h2>
-                        <p>{portal.description}</p>
-                        <a href={portal.link}>Visit Portal</a>
+                    <div className="portal-card" key={portal._id}>
+                        <img src={portal.portalImage.src} alt={portal.portalImage.alt} />
+                        <h2>{portal.portalTitle}</h2>
+                        <p>{portal.portalDescription}</p>
+                        <a href={`/${portal._id}`}>Visit Portal</a>
                     </div>
                 ))}
             </div>
-        </div>        
+        </div>
     )
 }
 
