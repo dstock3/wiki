7842f438e03d22d24e57a644a6fb0a08127664ad
David Stockdale <dstock3@gmail.com>
2023-09-28 20:57:40 -0500

    Added handler functions and useHistory hook to EditSectionPage component.

--- src/pages/dynamic/EditSectionPage.js
+++ src/pages/dynamic/EditSectionPage.js
@@ -1,11 +1,17 @@
 import { useEffect, useState } from 'react'
 import '../../styles/EditSectionPage.css'
 import axios from 'axios'
+import { useHistory } from 'react-router-dom'
 
 const EditSectionPage = ({ match, location, endpoint, title, csrfToken }) => {
     const [section, setSection] = useState({title: '', text: ''})
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const history = useHistory();
+    
+    const handleInputChange = (e, field) => {
+        setSection(prevState => ({ ...prevState, [field]: e.target.value }));
+    };
 
     useEffect(() => {
         axios.get(`${endpoint}/articles/${match.params.articleid}/${match.params.sectionid}`)
@@ -24,6 +30,35 @@ const EditSectionPage = ({ match, location, endpoint, title, csrfToken }) => {
         })
     }, [match.params.sectionid])
 
+    const handleSave = () => {
+        axios.put(`${endpoint}/articles/${match.params.articleid}/${match.params.sectionid}`, section, {
+            headers: {
+                'csrf-token': csrfToken
+            }
+        })
+        .then(response => {
+            history.push(`/${match.params.portalid}/article/${match.params.articleid}`);
+        })
+        .catch(error => {
+            console.error("Error updating section", error);
+        });
+    };
+    
+    const handleDelete = () => {
+        axios.delete(`${endpoint}/articles/${match.params.articleid}/${match.params.sectionid}`, {
+            headers: {
+                'csrf-token': csrfToken
+            }
+        })
+        .then(response => {
+            console.log("Section deleted successfully", response.data);
+            history.push(`/${match.params.portalid}/article/${match.params.articleid}`);
+        })
+        .catch(error => {
+            console.error("Error deleting section", error);
+        });
+    };
+    
     if (loading) return <div className="edit-section-page">Loading...</div>;
     if (error) return <div className="edit-section-page">Error: {error}</div>;
 
@@ -37,20 +72,22 @@ const EditSectionPage = ({ match, location, endpoint, title, csrfToken }) => {
                             type="text" 
                             placeholder="Section Title" 
                             value={section.title}
+                            onChange={e => handleInputChange(e, 'title')}
                         />
                     </div>
-                    <button className="delete-btn delete-section">Delete Section</button>
+                    <button className="delete-btn delete-section" onClick={handleDelete}>Delete Section</button>
                 </div>
                 <div className="edit-section-container">
                     <label>Section Content:</label>
                     <textarea 
                         placeholder="Section Content" 
                         value={section.text}
+                        onChange={e => handleInputChange(e, 'text')}
                     />
                 </div>
                 <div className="edit-section-btn-container">
-                    <button className="cancel-btn">Cancel</button>
-                    <button className="save-btn">Save</button>
+                    <button className="cancel-btn" >Cancel</button>
+                    <button className="save-btn" onClick={handleSave}>Save</button>
                 </div>
             </div>
         </div>
