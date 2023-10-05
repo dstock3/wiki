a2201dd8c92f8cc1721c1318c42236833328a87b
David Stockdale <dstock3@gmail.com>
2023-10-04 20:23:08 -0500

    Added conditional expression within PortalSidebar to check if there are any articles.

--- src/components/PortalSidebar.js
+++ src/components/PortalSidebar.js
@@ -5,8 +5,9 @@ const PortalSidebar = ({articles, portalId}) => {
   return (
     <div className="portal-side-menu side-menu-container">
       <h3>Articles</h3>
+      {articles.length === 0 && <p className="no-articles-found-msg">No articles found.</p>}
       <ul>
-        {articles.slice(0, 10).map((article, index) => (
+        {articles && articles.slice(0, 10).map((article, index) => (
           <li key={index}>
             <Link to={`/${portalId}/article/${article._id}`}>{article.title}</Link>
           </li>
