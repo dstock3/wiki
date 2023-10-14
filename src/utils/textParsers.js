590503d3cac5aad0cb4223bbb54842e18136a31a
David Stockdale <dstock3@gmail.com>
2023-10-14 13:07:35 -0500

    added functions to textParsers util file in order to parse any links and ensure they are absolute.

--- src/utils/textParsers.js
+++ src/utils/textParsers.js
@@ -9,8 +9,30 @@ function parseContentToHTML(content) {
     content = content.replace(referenceLinkRegex, (match, referenceNumber) => {
         return `<a class="reference-link" href="#reference-${referenceNumber}">[${referenceNumber}]</a>`;
     });
+    const contentWithParsedLinks = ensureLinksAreAbsolute(content);
 
-    return content;
+    return contentWithParsedLinks;
+}
+function ensureLinksAreAbsolute(htmlString) {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(htmlString, 'text/html');
+    
+    doc.querySelectorAll('a').forEach(anchor => {
+        const href = anchor.getAttribute('href');
+        anchor.setAttribute('href', ensureAbsoluteURL(href));
+    });
+
+    return doc.body.innerHTML;
+}
+
+function ensureAbsoluteURL(url) {
+    if (url.startsWith('http://') || url.startsWith('https://')) {
+        return url;
+    } else {
+        return 'http://' + url;
+    }
 }
 
 export { parseContentToHTML };
+
+
