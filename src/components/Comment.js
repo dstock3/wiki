1bf08490ec642ffe5e26e17a4554b951a1927485
dstock3 <dstock3@gmail.com>
2023-11-03 20:27:11 -0500

    added inline editing for comments
    Users can now edit their comments directly on the TalkPage. The edit
    operation is handled inline, and the component now includes error
    handling for edit operations.

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,16 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { formatDate } from '../utils/textParsers';
 import axios from 'axios';
+import DOMPurify from 'dompurify';
 
-const Comment = ({ index, comment, endpoint, currentUserId, articleAuthorId, topicId, articleId, onDeleteSuccess, csrfToken }) => {
+const Comment = ({
+    index,
+    comment,
+    endpoint,
+    currentUserId,
+    articleAuthorId,
+    topicId,
+    articleId,
+    onDeleteSuccess,
+    onEditSuccess,
+    csrfToken
+}) => {
+    const [editMode, setEditMode] = useState(false);
+    const [editedContent, setEditedContent] = useState(comment.content);
+    const [error, setError] = useState('');
+
     const isAuthor = comment.authorId === currentUserId;
     const canDelete = isAuthor || articleAuthorId === currentUserId;
-
-    useEffect(() => {
-        console.log("articleId" + articleId)
-    }, []);
-
 
     const handleDelete = async () => {
         const config = {
@@ -18,16 +29,29 @@ const Comment = ({ index, comment, endpoint, currentUserId, articleAuthorId, top
             headers: { 'csrf-token': csrfToken },
         };
         try {
-            const response = await axios.delete(`${endpoint}/talk/${articleId}/topics/${topicId}/comments/${comment._id}`, config);
+            await axios.delete(`${endpoint}/talk/${articleId}/topics/${topicId}/comments/${comment._id}`, config);
             onDeleteSuccess(comment._id);
         } catch (error) {
-            console.error("Error deleting comment:", error.response?.data?.error || error.message);
+            setError("Error deleting comment: " + (error.response?.data?.error || error.message));
         }
     };
 
+    const handleEditToggle = () => {
+        setEditMode(!editMode);
+    };
 
-    const handleEdit = () => {
-        // trigger a redirect to a page with a form for editing the comment.
+    const handleSave = async () => {
+        const config = {
+            withCredentials: true,
+            headers: { 'csrf-token': csrfToken },
+        };
+        try {
+            const response = await axios.put(`${endpoint}/talk/${articleId}/topics/${topicId}/comments/${comment._id}`, { content: editedContent }, config);
+            onEditSuccess(comment._id, response.data.updatedContent); // You need to implement this in the parent component
+            setEditMode(false);
+        } catch (error) {
+            setError("Error updating comment: " + (error.response?.data?.error || error.message));
+        }
     };
 
     return (
@@ -35,13 +59,28 @@ const Comment = ({ index, comment, endpoint, currentUserId, articleAuthorId, top
             <span className="comment-author">
                 <Link to={`/user/${comment.authorId}`}>{comment.author}</Link>:
             </span>
-            <span className="comment-content" dangerouslySetInnerHTML={{ __html: comment.content }} />
-            <div className="comment-date">{formatDate(comment.date)}</div>
-            {isAuthor && (
-                <button onClick={handleEdit} className="comment-edit-button">Edit</button>
+            {editMode ? (
+                <textarea
+                    className="comment-edit-textarea"
+                    value={editedContent}
+                    onChange={(e) => setEditedContent(e.target.value)}
+                />
+            ) : (
+                <span className="comment-content" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(comment.content) }} />
             )}
-            {canDelete && (
+            <div className="comment-date">{formatDate(comment.date)}</div>
+            {error && <div className="error">{error}</div>}
+            {isAuthor && !editMode && (
+                <button onClick={handleEditToggle} className="comment-edit-button">Edit</button>
+            )}
+            {canDelete && !editMode && (
                 <button onClick={handleDelete} className="comment-delete-button">Delete</button>
+            )}
+            {editMode && (
+                <>
+                    <button onClick={handleSave} className="comment-save-button">Save</button>
+                    <button onClick={handleEditToggle} className="comment-cancel-button">Cancel</button>
+                </>
             )}
         </div>
     );
