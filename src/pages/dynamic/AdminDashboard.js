5dc65f7a5bede716ebcc3ad4167d15423556cdc8
dstock3 <dstock3@gmail.com>
2023-11-12 02:16:12 -0600

    revised log fetching function and added jsx for log display

--- src/pages/dynamic/AdminDashboard.js
+++ src/pages/dynamic/AdminDashboard.js
@@ -1,41 +1,56 @@
 import React, { useState, useEffect } from 'react';
-import Loading from '../../components/Loading';
 import axios from 'axios';
+import Loading from '../../components/Loading';
 
 const AdminDashboard = ({ endpoint, title, csrfToken }) => {
     const [logs, setLogs] = useState([]);
     const [logsLoading, setLogsLoading] = useState(true);
     const [logsError, setLogsError] = useState('');
-    
+    const [searchQuery, setSearchQuery] = useState('');
+
     useEffect(() => {
         document.title = `${title} | Admin Dashboard`;
     }, [title]);
 
-    const fetchLogs = async () => {
-        axios.get(`${endpoint}/logs/`, { withCredentials: true })
-        .then(response => {
-            console.log(response.data);
-            setLogs(response.data.logs);
-            setLogsLoading(false);
-        })
-        .catch(err => {
+    const fetchLogs = async (logType = '') => {
+        setLogsLoading(true);
+        try {
+            const response = await axios.get(`${endpoint}/logs/${logType}`, { withCredentials: true });
+            setLogs(response.data.logs || response.data.errorLogs || response.data.infoLogs || response.data.warnLogs || response.data.searchResults);
+        } catch (err) {
             setLogsError(err.message);
-            setLogsLoading(false);
-        });    
+        }
+        setLogsLoading(false);
     };
 
-    const fetchErrorLogs = async () => {
-        axios.get(`${endpoint}/logs/error`, { withCredentials: true })
-        .then(response => {
-            console.log(response.data);
-            setLogs(response.data.errorLogs);
-            setLogsLoading(false);
-        })
-        .catch(err => {
-            setLogsError(err.message);
-            setLogsLoading(false);
-        });    
-    }
+    const handleSearch = async (e) => {
+        e.preventDefault();
+        if (searchQuery) {
+            fetchLogs(`search?query=${encodeURIComponent(searchQuery)}`);
+        }
+    };
+
+    const renderLogEntry = (log) => {
+        let message = log.message;
+        if (typeof message === 'object') {
+            // Stringify if message is an object
+            message = JSON.stringify(message, null, 2);
+        }
+    
+        return (
+            <li key={log.timestamp || log._id || Math.random()}>
+                <div className="log-level">
+                    <strong>Level:</strong> {log.level || 'N/A'}
+                </div>
+                <div className="log-message">
+                    <strong>Message:</strong> {message || 'N/A'}
+                </div>
+                <div className="log-service">
+                    <strong>Service:</strong> {log.service || 'N/A'}
+                </div>
+            </li>
+        );
+    };
 
     return (
         <div className="admin-dashboard">
@@ -43,17 +58,32 @@ const AdminDashboard = ({ endpoint, title, csrfToken }) => {
 
             <section className="logs-section">
                 <div className="logs-button-container">
-                    <button className="logs-button" onClick={fetchLogs}>Retrieve All Logs</button>
-                    <button className="logs-button" onClick={fetchErrorLogs}>Retreive Error Logs</button>
+                    <button className="logs-button" onClick={() => fetchLogs()}>Retrieve All Logs</button>
+                    <button className="logs-button" onClick={() => fetchLogs('error')}>Retrieve Error Logs</button>
+                    <button className="logs-button" onClick={() => fetchLogs('info')}>Retrieve Info Logs</button>
+                    <button className="logs-button" onClick={() => fetchLogs('warn')}>Retrieve Warn Logs</button>
                 </div>
-                
+
+                <form onSubmit={handleSearch} className="log-search-form">
+                    <input
+                        type="text"
+                        placeholder="Search logs..."
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                    />
+                    <button type="submit">Search</button>
+                </form>
+
                 {logsLoading ? (
                     <Loading loading={logsLoading} />
                 ) : logsError ? (
                     <p className="log-error">Error: {logsError}</p>
                 ) : (
-                    <div className="">
+                    <div className="logs-display">
                         <h2>Logs</h2>
+                        <ul>
+                            {logs.map(log => renderLogEntry(log))}
+                        </ul>
                     </div>
                 )}
             </section>
