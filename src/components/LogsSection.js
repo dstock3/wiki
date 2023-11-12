c370e546b87472d8a1a24b6ff66e5a3e29b45959
dstock3 <dstock3@gmail.com>
2023-11-12 14:35:32 -0600

    added renderLogEntry function to LogsSection component.

--- src/components/LogsSection.js
+++ src/components/LogsSection.js
@@ -1,6 +1,27 @@
 import Loading from "./Loading";
 
-const LogsSection = ({ handleSearch, searchQuery, setSearchQuery, fetchLogs, logsLoading, logs, logsError, renderLogEntry }) => {
+const LogsSection = ({ handleSearch, searchQuery, setSearchQuery, fetchLogs, logsLoading, logs, logsError }) => {
+    const renderLogEntry = (log) => {
+        let message = log.message;
+        if (typeof message === 'object') {
+            message = JSON.stringify(message, null, 2);
+        }
+    
+        return (
+            <li className="log-line-item" key={log.timestamp || log._id || Math.random()}>
+                <div className="log-level">
+                    <strong>Level:</strong> {log.level || 'N/A'}
+                </div>
+                <div className="log-message">
+                    <strong>Message:</strong> {message || 'N/A'}
+                </div>
+                <div className="log-service">
+                    <strong>Service:</strong> {log.service || 'N/A'}
+                </div>
+            </li>
+        );
+    };
+    
     return (
         <section className="logs-section">
         <h2 className="logs-head">Logs</h2>
--- src/pages/dynamic/AdminDashboard.js
+++ src/pages/dynamic/AdminDashboard.js
@@ -31,27 +31,6 @@ const AdminDashboard = ({ endpoint, title, csrfToken }) => {
         }
     };
 
-    const renderLogEntry = (log) => {
-        let message = log.message;
-        if (typeof message === 'object') {
-            message = JSON.stringify(message, null, 2);
-        }
-    
-        return (
-            <li className="log-line-item" key={log.timestamp || log._id || Math.random()}>
-                <div className="log-level">
-                    <strong>Level:</strong> {log.level || 'N/A'}
-                </div>
-                <div className="log-message">
-                    <strong>Message:</strong> {message || 'N/A'}
-                </div>
-                <div className="log-service">
-                    <strong>Service:</strong> {log.service || 'N/A'}
-                </div>
-            </li>
-        );
-    };
-
     return (
         <div className="admin-dashboard">
             <h1 className="admin-head">Admin Dashboard</h1>
@@ -64,9 +43,7 @@ const AdminDashboard = ({ endpoint, title, csrfToken }) => {
                 logsLoading={logsLoading}
                 logs={logs}
                 logsError={logsError}
-                renderLogEntry={renderLogEntry}
             />
-
         </div>
     );
 };
