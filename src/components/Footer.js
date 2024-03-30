9723000a0fdc54c8524d7feccba0bc72b64038a4
David Stockdale <dstock3@gmail.com>
2024-03-30 12:09:26 -0500

    properly centered footer section links.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,26 +7,32 @@ const Footer = ({title}) => {
     <div className="footer-container">
       <div className="footer-subcontainer">
         <div className="footer-section">
-          <h4>About</h4>
-          <ul>
-            <li><Link to="/wiki/about">About {title}</Link></li>
-            <li><Link to="/wiki/contact">Contact Us</Link></li>
-            <li><Link to="/wiki/disclaimer">Disclaimer</Link></li>
-          </ul>
+          <div className="footer-subsection">
+            <h4>About</h4>
+            <ul>
+              <li><Link to="/wiki/about">About {title}</Link></li>
+              <li><Link to="/wiki/contact">Contact Us</Link></li>
+              <li><Link to="/wiki/disclaimer">Disclaimer</Link></li>
+            </ul>
+          </div>
         </div>
         <div className="footer-section">
-          <h4>Contribute</h4>
-          <ul>
-            <li><Link to="/wiki/contribute">How to Contribute</Link></li>
-            <li><Link to="/wiki/donate">Donate</Link></li>
-          </ul>
+          <div className="footer-subsection">
+            <h4>Contribute</h4>
+            <ul>
+              <li><Link to="/wiki/contribute">How to Contribute</Link></li>
+              <li><Link to="/wiki/donate">Donate</Link></li>
+            </ul>
+          </div>
         </div>
         <div className="footer-section">
-          <h4>Legal</h4>
-          <ul>
-            <li><Link to="/wiki/terms">Terms of Use</Link></li>
-            <li><Link to="/wiki/privacy">Privacy Policy</Link></li>
-          </ul>
+          <div className="footer-subsection">
+            <h4>Legal</h4>
+            <ul>
+              <li><Link to="/wiki/terms">Terms of Use</Link></li>
+              <li><Link to="/wiki/privacy">Privacy Policy</Link></li>
+            </ul>
+          </div>
         </div>
       </div>
 
