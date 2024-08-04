ac66e50fe6c7961dbaf618ed29da2ad3f0988767
dstock3 <dstock3@gmail.com>
2024-08-04 10:14:39 -0500

    changed blog sidebar links to anchor elements instead of Link component to enable correct scrolling behavior.

--- src/components/BlogSidebar.js
+++ src/components/BlogSidebar.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const BlogSidebar = ({ links }) => {
-    const scrollTo = (id) => {
-        const element = document.getElementById(id);
-        if (element) {
-            element.scrollIntoView({ behavior: 'smooth' });
-        }
-    };
-
     return (
         <div className="blog-sidebar">
             <h4>Archive</h4>
             <ul>
                 {links.map((link, index) => (
                     <li key={index}>
-                        <Link to={link.href} onClick={() => scrollTo(link.href.substring(1))}>
-                            {link.text}
-                        </Link>
+                        <a href={link.href}>{link.text}</a>
                     </li>
                 ))}
             </ul>
